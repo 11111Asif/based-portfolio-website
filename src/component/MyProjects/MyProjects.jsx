5b86e2dbc256d527f0b15f4e1c28d2cb229f8db7
11111Asif <asif040502@gmail.com>
2025-07-27 23:05:52 -0700

    added to myprojeces step 3

--- src/component/MyProjects/MyProjects.jsx
+++ src/component/MyProjects/MyProjects.jsx
@@ -17,7 +17,7 @@ const MyProjects = () => {
                             className="lg:w-[530px] lg:h-[397px] w-[343px] h-[397px] rounded-lg" />
                         <div>
                             <h1 className="text-5xl font-bold text-white">01</h1>
-                            <h3 className="text-2xl font-bold text-white mt-3">Crypto Screener Application</h3>
+                            <h3 className="text-2xl font-bold text-white mt-4">Crypto Screener Application</h3>
                             <p className="py-6 text-[#71717A]">
                                 I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.
                             </p>
@@ -34,10 +34,27 @@ const MyProjects = () => {
                             className="lg:w-[530px] lg:h-[397px] w-[343px] h-[397px] rounded-lg " />
                         <div>
                             <h1 className="text-5xl font-bold text-white">02</h1>
-                            <h3 className="text-2xl font-bold text-white mt-3">Euphoria - Ecommerce (Apparels) Website Template</h3>
+                            <h3 className="text-2xl font-bold text-white mt-4">Euphoria - Ecommerce (Apparels) Website Template</h3>
                             <p className="py-6 text-[#71717A]">
                                 I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.
                                 when an unknown printer took a galley of type and scrambled it to specimen book.
+                            </p>
+                            <img src={read} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+             <div className=''>
+                <div className="hero ">
+                    <div className="hero-content flex-col px-4 lg:flex-row">
+                        <img
+                            src={image2}
+                            className="lg:w-[530px] lg:h-[397px] w-[343px] h-[397px] rounded-lg" />
+                        <div>
+                            <h1 className="text-5xl font-bold text-white">03</h1>
+                            <h3 className="text-2xl font-bold text-white mt-4">Blog Website Template</h3>
+                            <p className="py-6 text-[#71717A]">
+                                I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.
                             </p>
                             <img src={read} alt="" />
                         </div>
